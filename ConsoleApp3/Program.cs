b0862e605a12c6a70f838d2a33c3da3c1a47258e
fishman123456 <den9152@yandex.ru>
2023-11-09 01:10:59 +0700

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using ShambalaEncriptionLibrary_08_11_2023;
+namespace ConsoleApp3
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEncoder Encoder = new CustomEncoder();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Encoder.encode("Hello, World!"));
+        }
+    }
+}
---
+++ DinamicLinkTest_shambala08-11-2023/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DinamicLinkTest_shambala08_11_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly libAsm = Assembly.Load(
+                AssemblyName.GetAssemblyName("ShambalaEncriptionLibrary_08-11-2023.dll")
+                );
+            Console.WriteLine($"Загружена сборка {libAsm.FullName}");
+            Module libModule = libAsm.GetModule("ShambalaEncriptionLibrary_08-11-2023.dll");
+            Console.WriteLine($"Загружена модуль {libModule.FullyQualifiedName}");
+            
+            // получим тип нужного класса
+            Type encoderType = libModule.GetType("ShambalaEncriptionLibrary_08-11-2023.CustomEncoder");
+            Console.WriteLine($"Получен тип:{encoderType.FullName} ");
+            // создадим обьект полученного типа
+            object encoder = Activator.CreateInstance(encoderType);
+            // вызовем библиотечный метод
+            string line = "hello";
+            string result = encoderType.GetMethod("Encode").Invoke(encoder, new object[] {line}) as string;
+            Console.WriteLine($"Закодированная строка  {line} : {result}");
+            //Assembly libAsmt = Assembly.Load(
+            //    AssemblyName.GetAssemblyName("autocad_wf_layer_add_29-03-2023.dll")
+            //    );
+            Console.ReadKey();
+        }
+    }
+}
